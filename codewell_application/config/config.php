a6c35ec17768614e833727d4e7976a52ed281a20
itujono <itujono@gmail.com>
2016-08-02 09:23:43 +0700

    1. Okay wak.

--- codewell_application/config/config.php
+++ codewell_application/config/config.php
@@ -33,7 +33,7 @@ date_default_timezone_set('Asia/Jakarta');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/codewell/laundry/';
+$config['base_url'] = 'http://localhost/codewell/i-laundry/';
 
 /*
 |--------------------------------------------------------------------------
